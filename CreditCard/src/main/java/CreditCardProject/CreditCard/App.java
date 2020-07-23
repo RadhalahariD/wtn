7c2c74f438b78e8ff98df2d204749666c50003ef
Radha Lahari <Radha Lahari@DESKTOP-4FGT4CH>
2020-07-23 21:38:33 +0530

    WTN Credit Card

---
+++ CreditCard/src/main/java/CreditCardProject/CreditCard/App.java
@@ -0,0 +1,13 @@
+package CreditCardProject.CreditCard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ CreditCard/src/main/java/com/wipro/ccbill/exception/InputValidationException.java
@@ -0,0 +1,13 @@
+package com.wipro.ccbill.exception;
+
+ 
+
+@SuppressWarnings("serial")
+public class InputValidationException extends Exception {
+    
+    public String toString()
+    {
+    return "Invalid Input Data";    
+    }
+    
+}
